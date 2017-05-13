617d362210349b54c62e500041d7406ca22efc16
Nikhil Prabhakar <nprabhakar@linkedin.com>
2017-05-13 00:37:07 -0700

    First commit.

---
+++ src/main/java/com/nipra/fugue/App.java
@@ -0,0 +1,13 @@
+package com.nipra.fugue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
